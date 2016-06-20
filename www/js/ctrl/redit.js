cd01c8b61255c4915edfab93d28c10253ef38c9a
madhudskumar <madhudskumar@live.in>
2016-06-20 13:49:10 +0530

    fixes

--- www/js/ctrl/redit.js
+++ www/js/ctrl/redit.js
@@ -1,6 +1,6 @@
 app.controller('reditCtrl', function ($scope, $http) {
   $scope.stories = [];
-  $http.get('http://www.reddit.com/r/all/new/.json')
+  $http.get("http://www.reddit.com/r/all/new/.json")
     .success(function (response) {
       $scope.stories = response.data.children;
     })
